64eb8999f299a926d7b93f7e34ae8a67b9aaab2e
teletips <89345076+teletips@users.noreply.github.com>
2021-10-21 02:20:37 -0700

    DO NOT CHANGE ANYTHING IN THIS CODE

--- botstatus_teletips.py
+++ botstatus_teletips.py
@@ -56,7 +56,7 @@ async def main_teletips():
                 xxx_teletips += f"\n\n✔️ Last checked on: {last_update} ({TIME_ZONE})\n\n<i>♻️ Updates every 45min - Powered by Powerful Bot Status</i>"
                 await app.edit_message_text(int(CHANNEL_OR_GROUP_ID), MESSAGE_ID, xxx_teletips)
                 print(f"Last checked on: {last_update}")                
-                await asyncio.sleep(2700)
+                await asyncio.sleep(6300)
                         
 app.run(main_teletips())
 
